2e78154b44caa080c7e924e05f868a6553f5e9a5
AndreyChizh <info.andreychizh@gmail.com>
2013-02-27 13:58:40 +0200

    Add files

---
+++ Makefile
@@ -0,0 +1,6 @@
+# Set aliases for commands
+
+test:
+	@./node_modules/.bin/mocha --reporter spec
+
+.PHONY: test
---
+++ test/main.js
@@ -0,0 +1,43 @@
+var should = require("should");
+
+var shortlink = require("../lib/shortlink");
+
+describe('shortlink', function(){
+    describe('require module', function(){
+        it('should have "generateBase52" method', function(){
+            shortlink.should.have.property('generateBase52');
+            shortlink.generateBase52.should.be.an.instanceOf(Function);
+        });
+
+        it('should have "generateBase58" method', function(){
+            shortlink.should.have.property('generateBase58');
+            shortlink.generateBase58.should.be.an.instanceOf(Function);
+        });
+
+        it('should have "generate" method', function(){
+            shortlink.should.have.property('generate');
+            shortlink.generate.should.be.an.instanceOf(Function);
+        });
+
+        it('should have "decodeBase52" method', function(){
+            shortlink.should.have.property('decodeBase52');
+            shortlink.decodeBase52.should.be.an.instanceOf(Function);
+        });
+
+        it('should have "decodeBase58" method', function(){
+            shortlink.should.have.property('decodeBase58');
+            shortlink.decodeBase58.should.be.an.instanceOf(Function);
+        });
+
+        it('should have "encodeBase52" method', function(){
+            shortlink.should.have.property('encodeBase52');
+            shortlink.encodeBase52.should.be.an.instanceOf(Function);
+        });
+
+        it('should have "encodeBase58" method', function(){
+            shortlink.should.have.property('encodeBase58');
+            shortlink.encodeBase58.should.be.an.instanceOf(Function);
+        });
+
+    })
+});

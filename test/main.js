5209622f98ad6344b61b48a9de90ad404b477a74
AndreyChizh <info.andreychizh@gmail.com>
2013-03-01 00:01:33 +0200

    Change api

--- test/main.js
+++ test/main.js
@@ -3,56 +3,74 @@ var should = require("should");
 var shortlink = require("../lib/shortlink");
 
 describe('shortlink', function () {
-    describe('require module', function () {
-        it('should have "generateBase52" method', function () {
-            shortlink.should.have.property('generateBase52');
-            shortlink.generateBase52.should.be.an.instanceOf(Function);
-        });
 
-        it('should have "generateBase58" method', function () {
-            shortlink.should.have.property('generateBase58');
-            shortlink.generateBase58.should.be.an.instanceOf(Function);
-        });
+    describe('module', function () {
 
         it('should have "generate" method', function () {
             shortlink.should.have.property('generate');
             shortlink.generate.should.be.an.instanceOf(Function);
         });
 
-        it('should have "decodeBase52" method', function () {
-            shortlink.should.have.property('decodeBase52');
-            shortlink.decodeBase52.should.be.an.instanceOf(Function);
+
+        it('should have "generate" method', function () {
+            shortlink.should.have.property('decode');
+            shortlink.generate.should.be.an.instanceOf(Function);
         });
 
-        it('should have "decodeBase58" method', function () {
-            shortlink.should.have.property('decodeBase58');
-            shortlink.decodeBase58.should.be.an.instanceOf(Function);
-        });
 
-        it('should have "encodeBase52" method', function () {
-            shortlink.should.have.property('encodeBase52');
-            shortlink.encodeBase52.should.be.an.instanceOf(Function);
-        });
-
-        it('should have "encodeBase58" method', function () {
-            shortlink.should.have.property('encodeBase58');
-            shortlink.encodeBase58.should.be.an.instanceOf(Function);
+        it('should have "generate" method', function () {
+            shortlink.should.have.property('encode');
+            shortlink.generate.should.be.an.instanceOf(Function);
         });
 
     });
 
-    describe('"generateBase52" method', function () {
-        it('should return correct value (with no set length)', function () {
-            var result = shortlink.generateBase52();
+    describe('#generate method', function () {
+
+        it('should return correct type value (with no set length)', function () {
+            var result = shortlink.generate();
             result.should.be.a('string');
             result.should.have.length(5);
         });
 
-        it('should return correct value (with set length 10)', function () {
-            var result = shortlink.generateBase52(10);
+        it('should return correct type value (with set length 10)', function () {
+            var result = shortlink.generate(10);
             result.should.be.a('string');
             result.should.have.length(10);
         });
 
     });
+
+    describe('#encode method', function () {
+
+        it('should return correct value', function () {
+            var str = shortlink.encode(1234567890);
+            str.should.be.a('string');
+            str.should.equal('2T6u2h');
+        });
+
+        it('should return correct value (example from flickr.com)', function () {
+            var str = shortlink.encode(8515010570);
+            str.should.be.a('string');
+            str.should.equal('dYrDZ5');
+        });
+
+    });
+
+    describe('#decode method', function () {
+
+        it('should return correct value', function () {
+            var num = shortlink.decode('2T6u2h');
+            num.should.be.a('number');
+            num.should.equal(1234567890);
+        });
+
+        it('should return correct value (example from flickr.com)', function () {
+            var num = shortlink.decode('dYrDZ5');
+            num.should.be.a('number');
+            num.should.equal(8515010570);
+        });
+
+    });
+
 });

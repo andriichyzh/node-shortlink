1094e6d065fef108ad61a74f5e382b136c793c15
Andrey Chizh <achizh@Andreys-MacBook-Pro.local>
2014-05-05 23:27:34 +0300

    Fix tests (should api)

--- Makefile
+++
@@ -1,6 +0,0 @@
-# Set aliases for commands
-
-test:
-	@./node_modules/.bin/mocha --reporter spec
-
-.PHONY: test
--- example/demo.js
+++ example/demo.js
@@ -4,19 +4,19 @@ setInterval(function() {
 
     // Generate random shortlink
     var randomStr = shortlink.generate(6);
-    console.log('Generate random shortlink: ' + randomStr);
+    console.log('Generate random shortlink:', randomStr);
 
     // Decode random shortlink to int
     var randomNum = shortlink.decode(randomStr);
-    console.log('Decode this shortlink to int: ' + randomNum);
+    console.log('Decode this shortlink to int:', randomNum);
 
     // Encode this int to shortlink
     var str = shortlink.encode(randomNum);
-    console.log('Encode this int to shortlink: ' + str);
+    console.log('Encode this int to shortlink:', str);
 
     // Decode this shortlink to int
     var num = shortlink.decode(str);
-    console.log('Decode this shortlink to int: ' + num);
+    console.log('Decode this shortlink to int:', num);
 
     console.log('--------------');
 
